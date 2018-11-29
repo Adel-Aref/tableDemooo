04b3ace6a9144aaf028a8f5db4dcf27d9c54dc0c
Adel <adel.aref16@gmail.com>
2018-11-29 18:43:28 +0200

    finsh home screen design  commit

--- Mind/ViewController.swift
+++ Mind/ViewController.swift
@@ -11,15 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var firstView: ANCustomView!
     
+    @IBOutlet weak var viewdateApperrance: UIView!
     
     let path = UIBezierPath()
     override func viewDidLoad() {
         super.viewDidLoad()
-        path.addLine(to: CGPoint(x: firstView.frame.size.width, y: firstView.frame.size.height))
-        path.close()
-        firstView.layer.shadowPath = path.cgPath
+//        path.addLine(to: CGPoint(x: firstView.frame.size.width, y: firstView.frame.size.height))
+//        path.close()
+//        firstView.layer.shadowPath = path.cgPath
     }
-
-
+// MARK :- date from  Action
+    @IBAction func dateFromPressed(_ sender: Any) {
+        viewdateApperrance.isHidden = false
+    }
+// MARK :- date from  Action
+    @IBAction func dateToPressed(_ sender: Any) {
+         viewdateApperrance.isHidden = false
+    }
+    // MARK :- Done Action
+    @IBAction func DateSelectedPressed(_ sender: Any) {
+        viewdateApperrance.isHidden = true
+    }
+    
 }
 
